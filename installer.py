22669d38b6ab989f8ea530c9489096da92a54349
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2024-03-25 14:55:45 +0530

    Update installer.py
    spelling error corrected

--- installer.py
+++ installer.py
@@ -1,3 +1,3 @@
 import os
 os.system("pip install -r reqdment.txt")
-# initial commit : can get jvascript unobfudcated code for index.html in app.html  https://github.com/Sh1vam/Diabetes_Prediction/commit/dd09e311ae6d2202816b34735500522be161d3ad
+# initial commit : can get jvascript unobfuscated code for index.html in app.html  https://github.com/Sh1vam/Diabetes_Prediction/commit/dd09e311ae6d2202816b34735500522be161d3ad
